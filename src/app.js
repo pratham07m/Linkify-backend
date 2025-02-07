95ab57e519fd21cd3f236671ff04a5baf7e4d99f
Pratham_Mahant <pratham2004m@gmail.com>
2025-02-08 00:49:17 +0530

    chat application implement

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database.js");
 const app = express();
 const cookieParser = require("cookie-parser"); //user for reading cookies
 const cors = require("cors");
+const http = require("http")
 require("dotenv").config();
 
 // const corsOptions = {
@@ -32,17 +33,23 @@ const profileRouter = require("./routes/profile.js");
 const requestRouter = require("./routes/request.js");
 
 const userRouter = require("./routes/user.js");
+const initializeSocket = require("./utils/socket.js");
+const chatRouter = require("./routes/chat.js");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/" , chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("DB connect");
 
-    app.listen(7777, () => {
+    server.listen(7777, () => {
       console.log("server startd");
     });
   })
